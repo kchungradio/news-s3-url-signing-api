a710bd500fdc0b2d76868c6de32040a637e0742c
Tim Phillips <tim.p.phillips@gmail.com>
2017-03-15 14:52:52 -0700

    add POST

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
-const { getStories } = require('./lib/stories')
+const { json, createError } = require('micro')
 
-const fetchStories = async () => {
-  try {
-    const stories = await getStories()
-    return stories.Items
-  } catch (err) {
-    console.error(err)
-  }
-}
+const { getStories, createStory } = require('./lib/stories')
 
 module.exports = async function (req, res) {
-  return fetchStories()
+  if (process.env.NODE_ENV !== 'production') {
+    res.setHeader('Access-Control-Allow-Origin', '*')
+  }
+
+  switch (req.method) {
+    case 'GET':
+      const stories = await getStories()
+      return stories.Items
+    case 'POST':
+      // validate jwt here
+      const data = await json(req)
+      const story = data.story
+      const result = await createStory(story)
+      return result
+    default:
+      throw createError(405, 'Invalid method')
+  }
 }
