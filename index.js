1069260a561eeb64b4d226ab96013ca90a818a0d
Tim Phillips <tim.p.phillips@gmail.com>
2017-08-24 11:35:45 -0700

    lookup stories by author slug

--- index.js
+++ index.js
@@ -1,22 +1,28 @@
-const { json, createError } = require('micro')
+const url = require('url')
+const { createError } = require('micro')
 
-const { getStoriesList, createStory } = require('./lib/stories')
+const { stories } = require('./lib/db')
 
-module.exports = async function (req, res) {
+module.exports = async (req, res) => {
   if (process.env.NODE_ENV !== 'production') {
     res.setHeader('Access-Control-Allow-Origin', '*')
   }
 
   switch (req.method) {
     case 'GET':
-      const stories = await getStoriesList()
-      return stories.Items
-    case 'POST':
-      // validate jwt here
-      const data = await json(req)
-      const story = data.story
-      const result = await createStory(story)
-      return result
+      // parse url for query params
+      const { authorSlug } = url.parse(req.url, true).query
+      // query or scan the db depending on query params
+      const result = authorSlug
+        ? await stories.query('author', '=', authorSlug)
+        : await stories.scan()
+      return result.Items
+    // case 'POST':
+    //   // validate jwt here
+    //   const data = await json(req)
+    //   const story = data.story
+    //   const result = await createStory(story)
+    //   return result
     default:
       throw createError(405, 'Invalid method')
   }
