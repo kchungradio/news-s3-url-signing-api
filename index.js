1726963c56fd0ce31cb8bbddf2be7e9d15e0e2e3
Tim Phillips <tim.p.phillips@gmail.com>
2017-08-28 18:15:21 -0700

    refactor stories methods

--- index.js
+++ index.js
@@ -1,33 +1,6 @@
 const { router, get, post } = require('microrouter')
-const url = require('url')
-const { json } = require('micro')
 
-const { allowAllOriginsOutsideProduction } = require('./lib/origins')
-const { stories } = require('./lib/db')
-
-const getStories = async (req, res) => {
-  allowAllOriginsOutsideProduction(res)
-
-  // parse url for query params
-  const { authorSlug } = url.parse(req.url, true).query
-  // query or scan the db depending on query params
-  const result = authorSlug
-    ? await stories.query('authorSlug', '=', authorSlug)
-    : await stories.scan()
-  return result.Items
-}
-
-const postStory = async (req, res) => {
-  allowAllOriginsOutsideProduction(res)
-
-  // TODO: authenticate
-  const story = await json(req)
-  // TODO: validate story
-  // TODO: make sure the owner matches the session creds
-  story.createdAt = new Date().toISOString()
-  const result = await stories.add(story)
-  return result
-}
+const { getStories, postStory } = require('./lib/stories')
 
 module.exports = router(
   get('/', getStories),
