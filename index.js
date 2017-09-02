0999f5be47f8face10c7160c75cd9ebfa941467b
Tim Phillips <tim.p.phillips@gmail.com>
2017-09-02 09:17:43 -0700

    create signed urls for browser to s3 uploads

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
-const { router, get, post } = require('microrouter')
+const { router, get, post, options } = require('microrouter')
 
 const { getStories, postStory } = require('./lib/stories')
+const { getSignedUrl } = require('./lib/s3')
+const { preflight } = require('./lib/cors')
 
 module.exports = router(
   get('/', getStories),
-  post('/', postStory)
+  post('/', postStory),
+  get('/s3/sign', getSignedUrl),
+  options('/s3/sign', preflight)
 )
