bd5c6a6b1c6e119451d08fb8112fd94f6e6bc2f6
Tim Phillips <tim.p.phillips@gmail.com>
2017-09-05 22:34:45 -0700

    edit story

--- index.js
+++ index.js
@@ -1,12 +1,38 @@
-const { router, get, post, options } = require('microrouter')
+const {
+  router,
+  get,
+  post,
+  put,
+  options
+} = require('microrouter')
 
-const { getStories, postStory } = require('./lib/stories')
+const {
+  getStories,
+  getStory,
+  createStory,
+  replaceStory
+} = require('./lib/stories')
 const { getSignedUrl } = require('./lib/s3')
 const { preflight } = require('./lib/cors')
 
+// TODO: authenticate in lib/stories
+// TODO: authenticate in lib/s3
+// TODO: paginate
+//
+// primary key: authorSlug
+// sort key: createdAt
+// consider changing primary key to authorId in case of name change
+
 module.exports = router(
-  get('/', getStories),
-  post('/', postStory),
+  get('/', getStories), // allows ?authorSlug=author-slug
+  get('/:titleSlug', getStory),
+
+  post('/', createStory),
+  put('/:titleSlug', replaceStory),
+
   get('/s3/sign', getSignedUrl),
+
+  options('/', preflight),
+  options('/:titleSlug', preflight),
   options('/s3/sign', preflight)
 )
